07c05502e89f3ce174b81d13980557a359d54a6a
Nicholas Bollweg <nbollweg@continuum.io>
2016-06-08 18:03:33 -0500

    squelch link script output

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -1,1 +1,1 @@
-"%PREFIX%\Scripts\jupyter-nbextension.exe" enable bqplot --py --sys-prefix && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-nbextension.exe" enable bqplot --py --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
